fc016c943b65e484c88d2bd6978123dd5183e7f8
Gabrieleabu23 <kaidgabriele23@gmail.com>
2023-11-27 16:35:46 +0100

    add: script equal word or not

--- palindromo/js/script.js
+++ palindromo/js/script.js
@@ -1,6 +1,27 @@
 const parola= prompt("Inserisci parola");
 const vet= [];
-for( let i=parola.length; i>0; i--){
-    vet[i]=parola[i-1];
+let g=parola.length;
+let results=0;
+for( let i=0; i<(parola.length/2); i++){
+    vet[i]=parola[i];
     console.log(vet[i]);
+    while(g-1 >((parola.length/ 2 )-1)){
+        vet[g]=parola[g-1];
+        console.log(vet[g]);
+    //    console.log("i",vet[i]);
+    //    console.log("g",vet[g]);
+       if(vet[g] !== vet[i]){
+        results--;
+       }
+       else{
+        results++;
+       }
+       g--;
+       break
+    }
+    
 }
+if(boh === Math.round((parola.length/2))){
+    console.log("uguale");
+}
+
