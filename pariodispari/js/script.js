932f8dc0e8e5586adba49c779366d8c2d0a45b72
Gabrieleabu23 <kaidgabriele23@gmail.com>
2023-11-27 18:38:41 +0100

    fix: console log order and content

--- pariodispari/js/script.js
+++ pariodispari/js/script.js
@@ -1,20 +1,20 @@
 let userNumber=parseInt( prompt("Inserisci un numero compreso da 1 a 5"));
 let randomN=0; let userPcsum = 0;
 randomN= pcNumber(randomN);
-// DEBUG
-console.log(randomN);
+
 userPcsum= sumPcUser(userNumber,randomN)
-// DEBUG
-console.log(userPcsum);
 while(userNumber <1 || userNumber >5){
     alert("Numero non valido!")
     userNumber= parseInt( prompt("Inserisci un numero compreso da 1 a 5"));
 }
+// DEBUG
+console.log("Numero generato da PC: ",randomN);
 console.log("Numero utente: ",userNumber);
+console.log("Somma numeri: ",userPcsum);
 
-
-function sumPcUser(){
-
+function sumPcUser(userNumber,randomN){
+    let sum= userNumber + randomN;
+    return sum;
 }
 
 // FUNZIONE PER GENERARE IL NUMERO CASUALE DEL PC , COMPRESO TRA 1 E 5
