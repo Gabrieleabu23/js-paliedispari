bce3a5a897b5bca7a9870232bc0859121aab62a6
Gabrieleabu23 <kaidgabriele23@gmail.com>
2023-11-27 18:36:35 +0100

    fix: organize variable to call function

--- pariodispari/js/script.js
+++ pariodispari/js/script.js
@@ -1,14 +1,21 @@
 let userNumber=parseInt( prompt("Inserisci un numero compreso da 1 a 5"));
-const randomN=0;
-numberPC= pcNumber(randomN);
+let randomN=0; let userPcsum = 0;
+randomN= pcNumber(randomN);
 // DEBUG
-console.log(numberPC);
+console.log(randomN);
+userPcsum= sumPcUser(userNumber,randomN)
+// DEBUG
+console.log(userPcsum);
 while(userNumber <1 || userNumber >5){
     alert("Numero non valido!")
     userNumber= parseInt( prompt("Inserisci un numero compreso da 1 a 5"));
 }
 console.log("Numero utente: ",userNumber);
 
+
+function sumPcUser(){
+
+}
 
 // FUNZIONE PER GENERARE IL NUMERO CASUALE DEL PC , COMPRESO TRA 1 E 5
 function pcNumber(randomN){
