b17afb79864487503d8d1894f595561c57042dbd
Gabrieleabu23 <kaidgabriele23@gmail.com>
2023-11-27 18:44:21 +0100

    add: function pari o dispari for final print in console log

--- pariodispari/js/script.js
+++ pariodispari/js/script.js
@@ -1,17 +1,31 @@
 let userNumber=parseInt( prompt("Inserisci un numero compreso da 1 a 5"));
-let randomN=0; let userPcsum = 0;
-randomN= pcNumber(randomN);
+let randomN=0; let userPcsum = 0;let pariDispari=0;
 
+randomN= pcNumber(randomN);
 userPcsum= sumPcUser(userNumber,randomN)
+pariDispari= pariodispari(userPcsum);
 while(userNumber <1 || userNumber >5){
     alert("Numero non valido!")
     userNumber= parseInt( prompt("Inserisci un numero compreso da 1 a 5"));
 }
-// DEBUG
+// DEBUG \\
 console.log("Numero generato da PC: ",randomN);
 console.log("Numero utente: ",userNumber);
 console.log("Somma numeri: ",userPcsum);
+console.log("Risultato:",pariDispari);
 
+// FUNZIONE PARI O DISPARI
+function pariodispari(userPcsum){
+    if(userPcsum % 2){
+        return "Dispari";
+    }
+    else{
+        return "Pari";
+    }
+}
+
+
+// FUNZIONE PER SOMMARE I NUMERI
 function sumPcUser(userNumber,randomN){
     let sum= userNumber + randomN;
     return sum;
